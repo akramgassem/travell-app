f4da9a0f7408f7bc8812990a4a0ecc45a495959c
akramgassem <akramgassem@gmail.com>
2020-03-26 10:55:25 +0100

    feat: - add common js config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,55 +1,8 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-  entry: './src/client/js/index.js',
+module.exports = merge(common, {
   mode: 'development',
-  devtool: 'source-map',
   stats: 'normal',
-  output: {
-    libraryTarget: 'var',
-    library: 'APP'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader'
-          }
-        ]
-      },
-      {
-        test: '/.js$/',
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({ filename: '[name].css' }),
-    new HtmlWebpackPlugin({
-      template: './src/client//views/index.html'
-    }),
-    new CleanWebpackPlugin({
-      // Simulate the removal of files
-      dry: true,
-      // Write Logs to Console
-      verbose: false,
-      // Automatically remove all unused webpack assets on rebuild
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false
-    })
-  ]
-};
+  devtool: 'source-map'
+});
